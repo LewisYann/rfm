68eeb6a6f51fcdd16081b3de598840acd133f113
Lewis <lewis@gmail.com>
2022-01-09 11:13:38 +0000

    last commit before refactor

--- src/layouts/Admin.js
+++ src/layouts/Admin.js
@@ -9,14 +9,10 @@ import { makeStyles } from "@material-ui/core/styles";
 import Navbar from "components/Navbars/Navbar.js";
 import Footer from "components/Footer/Footer.js";
 import Sidebar from "components/Sidebar/Sidebar.js";
- 
 import routes from "routes.js";
-
 import styles from "assets/jss/material-dashboard-react/layouts/adminStyle.js";
-
 import bgImage from "assets/img/sidebar-2.jpg";
 import logo from "assets/img/reactlogo.png";
-
 let ps;
 
 const switchRoutes = (
